656bc4f81049c58b5a2c7e24268f814451ade871
Andrew Friedrich <friedram@comcast.net>
2022-07-09 13:31:00 -0700

    Creating new Heroku base JamSession 07/09/2022
    Updating for typing_extension compatibility

--- jamSession/settings.py
+++ jamSession/settings.py
@@ -122,16 +122,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+#STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 STATIC_ROOT = [os.path.join(BASE_DIR, 'static')]
 
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+#STATICFILES_DIRS = (    os.path.join(BASE_DIR, 'static'),)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
