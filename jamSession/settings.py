b9d6d57a974c626714e0a8be600674eb5d60cb25
Andrew Friedrich <friedram@comcast.net>
2022-07-09 13:51:10 -0700

    Creating new Heroku base JamSession 07/09/2022
    Updating for typing_extension compatibility

--- jamSession/settings.py
+++ jamSession/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
-4STATIC_ROOT = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = [os.path.join(BASE_DIR, 'static')]
 
 #STATIC_ROOT = '/static/'
 
