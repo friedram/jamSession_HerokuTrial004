2ce854c397407447252f190bba5ad91768f25eb2
Andrew Friedrich <friedram@comcast.net>
2022-07-09 14:12:14 -0700

    Creating new Heroku base JamSession 07/09/2022
    Updating for typing_extension compatibility

--- jamSession/settings.py
+++ jamSession/settings.py
@@ -81,7 +81,8 @@ WSGI_APPLICATION = 'jamSession.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+        #'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
@@ -123,7 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+#STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 #STATIC_ROOT = [os.path.join(BASE_DIR, 'static')]
 
